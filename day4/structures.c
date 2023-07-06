679a6e2290f4df6e3f0949ecb150bb452fb6774e
Paramveer Udawat <udawat11paramveer@gmail.com>
2023-07-06 22:57:26 +0530

    edited the previous code

--- day4/structures.c
+++ day4/structures.c
@@ -1,24 +1,24 @@
-// #include<stdio.h>
-// struct employee
-// {
-//     int id;
-//     char name;
-//     float marks;
-// }; //a,b,c; //these are the variables of struct
-// // struct employee a; like this also you can define variable
+#include<stdio.h>
+struct employee
+{
+    int id;
+    char name;
+    float marks;
+}; //a,b,c; //these are the variables of struct
+// struct employee a; like this also you can define variable
 
-// int main()
-// {
-//     struct employee m; //you can define variable here also
-//     //rn we using the above variable so i commented the other variables
-//     m.id = 12;
-//     m.name = 'a';
-//     m.marks = 2.4; //like this defined_variable.assigned_variable is the format of providing the data
+int main()
+{
+    struct employee m; //you can define variable here also
+    //rn we using the above variable so i commented the other variables
+    m.id = 12;
+    m.name = 'a';
+    m.marks = 2.4; //like this defined_variable.assigned_variable is the format of providing the data
 
-//     printf("Marks : %f\n",m.marks); //it will print the marks
-//     printf("Name : %c\n",m.name);
-//     return 0;
-// }
+    printf("Marks : %f\n",m.marks); //it will print the marks
+    printf("Name : %c\n",m.name);
+    return 0;
+}
 
 #include <stdio.h>
 struct employee
